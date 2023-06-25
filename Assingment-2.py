c7e18bedd8a44efb036e65bcc0a7db6d32948805
Mohammad Ashir Alam <mdashir81@gmail.com>
2023-06-25 17:38:57 +0530

    Update Assingment-2.py

--- Assingment-2.py
+++ Assingment-2.py
@@ -1,1 +1,84 @@
+# Q1. How do you comment code in Python? What are the different types of comments?
+# Comments in Python is usually described by "#" hash tag like 
+print("Below Commment")
+#print("Hello World"). This is Comment
 
+# There are two types Comments:
+# Inline Comment: Used for a single-line comment, marked by a start comment token till the end of the line.
+# Block Comment: Used for a multi-line comment, marked by a pair of start-end comment tokens.
+
+# Q2. What are variables in Python? How do you declare and assign values to variables?
+# Variables are some alphabet of English That describe the values assigned to it.
+# Types of Declaring variable and assigning values:
+# a=3
+# a_1=4
+# abc=4
+# a_bc=9
+
+# Q3. How do you convert one data type to another in Python?
+a=2
+print(float(a))
+#Output
+#2.0 it will be convert int data type to float
+
+# Q4. How do you write and execute a Python script from the command line?
+# To write and exceute Python Script from command line:
+# 1. Open Command line with Administrator or Simply.
+# 2. Type python or python3 for version followed by >> or in (shell) $.
+# 3. Then type filename with extension py like filename_extension(py) 
+# 4. Example $python Hello.py or $python3 Hello.py
+# 5. Output will be displayed.
+
+# Q5. Given a list my_list = [1, 2, 3, 4, 5], write the code to slice the list and obtain the sub-list [2, 3].
+
+my_list=[1,2,3,4,5]
+print(my_list[1:3])
+
+# Q6. What is a complex number in mathematics, and how is it represented in Python?
+# The Complex no. in mathematics is simply a imaginary and real no. 
+# where Complex numbers are the numbers that are expressed in the form of a+ib 
+# where, a,b are real numbers and 'i' is an imaginary number
+
+
+# and in python
+
+import math
+z = 5+3j
+print("The real part of complex number is : ", end="")
+print(z.real)
+
+print("The imaginary part of complex number is : ", end="")
+print(z.imag)
+
+# Q7. What is the correct way to declare a variable named age and assign the value 25 to it?
+
+age=25
+print("Age",age)
+
+# Q8. Declare a variable named price and assign the value 9.99 to it. What data type does this variable belong to?
+
+price=9.99
+#To know data type of variable we use type().
+print(type(price))
+
+#Output
+#<class 'float'>
+
+# Q9. Create a variable named name and assign your full name to it as a string. How would you print the value of this variable?
+name="Mohammad Ashir Alam"
+print(name)
+#Output
+#Mohammad Ashir Alam
+
+# Q10. Given the string "Hello, World!", extract the substring "World".
+s="Hello, World!"
+# To extract the substring of given string we use slicing
+
+print(s[7:-1])
+print(s[7:12])
+
+# Q11. Create a variable named "is_student" and assign it a boolean value indicating whether you are currently a student or not.
+is_student=True
+
+print(type(is_student))
+print(is_student)
